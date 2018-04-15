cb03c490d4b6efe75d582381f6b6e3b46a42c79e
Sergio Hinojosa <hin19sergio97@gmail.com>
2018-04-15 10:56:43 -0500

    Packtesting?

---
+++ GSO/src/GSOData/GDate.java
@@ -0,0 +1,22 @@
+package GSOData;
+import java.util.*;
+import java.time.*;
+
+public class GDate {
+	
+	public String SDates;
+	public int week;
+	public int year;
+	public int month;
+	public int day;
+	
+	public GDate(int mon, int da, int yea)
+	{
+		month = mon;
+		day = da;
+		year = yea;
+		
+	}
+	
+	
+}
